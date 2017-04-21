3ea4af05e0bcb43797d8c795c6de2c316786300c
Ellen Shapiro <designatednerd@gmail.com>
2017-04-21 10:54:57 -0500

    add parrot kit

---
+++ ParrotKit/ParrotKit.h
@@ -0,0 +1,19 @@
+//
+//  ParrotKit.h
+//  ParrotKit
+//
+//  Created by Ellen Shapiro (Work) on 4/21/17.
+//  Copyright © 2017 Designated Nerd Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParrotKit.
+FOUNDATION_EXPORT double ParrotKitVersionNumber;
+
+//! Project version string for ParrotKit.
+FOUNDATION_EXPORT const unsigned char ParrotKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParrotKit/PublicHeader.h>
+
+
