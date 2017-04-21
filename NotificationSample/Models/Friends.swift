a4b5f8eaaacb1f893d830c550639efab14bb6fe4
Ellen Shapiro <designatednerd@gmail.com>
2017-04-21 10:51:53 -0500

    add friends, and friend data source

---
+++ NotificationSample/Models/Friends.swift
@@ -0,0 +1,19 @@
+//
+//  Friends.swift
+//  NotificationSample
+//
+//  Created by Ellen Shapiro (Work) on 4/21/17.
+//  Copyright © 2017 Designated Nerd Software. All rights reserved.
+//
+
+import Foundation
+
+struct Friends {
+
+    static let mine = Friends()
+    
+    var parrots = [PartyParrot]()
+
+    
+    
+}
--- NotificationSample/Models/ParrotCollectionViewDataSource.swift
+++ NotificationSample/Models/ParrotCollectionViewDataSource.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ParrotCollectionViewDataSource: NSObject, UICollectionViewDataSource {
     
+    var parrots: [PartyParrot] {
+        fatalError("You'd think this would be a good place for a protocol, but apparently protocols and objective-c interop make all child deities cry, so subclasses must override this var.")
+    }
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return Party.current.parrots.count
+        return self.parrots.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -23,9 +27,24 @@ class ParrotCollectionViewDataSource: NSObject, UICollectionViewDataSource {
             fatalError("This is not my beautiful cell!")
         }
         
-        let parrot = Party.current.parrots[indexPath.item]
+        let parrot = self.parrots[indexPath.item]
         cell.parrot = parrot
         
         return cell
     }
+}
+
+class PartyCollectionViewDataSource: ParrotCollectionViewDataSource {
+
+    override var parrots: [PartyParrot] {
+        return Party.current.parrots
+    }
+}
+
+class FriendsCollectionViewDataSource: ParrotCollectionViewDataSource {
+
+    override var parrots: [PartyParrot] {
+        return Friends.mine.parrots
+    }
+    
 }
