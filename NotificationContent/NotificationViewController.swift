2f0aa31d27c9d6e08c6205c18da127dfdfde782c
Ellen Shapiro <designatednerd@gmail.com>
2017-04-21 19:40:16 -0500

    UI for notification extension

--- NotificationContent/NotificationViewController.swift
+++ NotificationContent/NotificationViewController.swift
@@ -6,20 +6,41 @@
 //  Copyright © 2016 Designated Nerd Software. All rights reserved.
 //
 
+import ParrotKit
 import UIKit
 import UserNotifications
 import UserNotificationsUI
 
-class NotificationViewController: UIViewController, UNNotificationContentExtension {
+class NotificationViewController: UIViewController {
 
-    @IBOutlet var label: UILabel?
+    @IBOutlet fileprivate var parrotImageView: UIImageView!
+    @IBOutlet fileprivate var titleLabel: UILabel!
+    @IBOutlet fileprivate var contentLabel: UILabel!
+    @IBOutlet private var partyCollectionView: UICollectionView!
+    @IBOutlet private var friendsCollectionView: UICollectionView!
+    
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any required interface initialization here.
+        
+        ParrotCollectionViewCell.register(with: self.partyCollectionView)
+        ParrotCollectionViewCell.register(with: self.friendsCollectionView)
     }
-    
+}
+
+//MARK: - Notification Content Extension
+
+extension NotificationViewController: UNNotificationContentExtension {
     func didReceive(_ notification: UNNotification) {
-        self.label?.text = notification.request.content.body
+        self.contentLabel.text = notification.request.content.body
+        self.titleLabel.text = notification.request.content.title
+        
+        guard let parrot = PartyParrot(userInfo: notification.request.content.userInfo) else {
+            assertionFailure("Couldn't create parrot from user info!")
+            return
+        }
+        
+        self.parrotImageView.image = parrot.gif.animated
     }
 }
